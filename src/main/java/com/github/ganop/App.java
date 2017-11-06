36ee228bb6fe78bdaa79c8d85d35bf66bdbf412a
ganop <petelov.gani@gmail.com>
2017-11-06 20:49:37 +0000

    Setup a Maven project mostly successfully

---
+++ src/main/java/com/github/ganop/App.java
@@ -0,0 +1,10 @@
+package com.github.ganop;
+
+public class App {
+    private static final String TEXT = "Hello World!";
+
+
+    public static void main(String[] args) {
+        System.out.println(TEXT);
+    }
+}
---
+++ test/main/java/com/github/ganop/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.ganop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testMainPrints() throws Exception {
+        assertTrue(true);
+    }
+
+}
