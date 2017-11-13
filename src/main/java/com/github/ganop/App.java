6e3141e684f072a1e30222fd423f8e64adfd8b56
ganop <petelov.gani@gmail.com>
2017-11-13 20:59:46 +0000

    Some responses using Apache Http Client

--- src/main/java/com/github/ganop/App.java
+++ src/main/java/com/github/ganop/App.java
@@ -1,10 +1,41 @@
 package com.github.ganop;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class App {
     private static final String TEXT = "Hello World!";
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         System.out.println(TEXT);
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(new URI("https://www.google.co.uk/"));
+        // The underlying HTTP connection is still held by the response object
+// to allow the response content to be streamed directly from the network socket.
+// In order to ensure correct deallocation of system resources
+// the user MUST call CloseableHttpResponse#close() from a finally clause.
+// Please note that if response content is not fully consumed the underlying
+// connection cannot be safely re-used and will be shut down and discarded
+// by the connection manager.
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            entity1.writeTo(new BufferedOutputStream(System.out));
+            EntityUtils.consume(entity1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- test/main/java/com/github/ganop/AppTest.java
+++ test/main/java/com/github/ganop/AppTest.java
@@ -2,11 +2,16 @@ package com.github.ganop;
 
 import org.junit.Test;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.net.URL;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
     @Test
-    public void testMainPrints() throws Exception {
+    public void HttpRequestGetTest() throws Exception {
+        URL google = new URL("https", "www.google.co.uk", "/");
+
         assertTrue(true);
     }
 
